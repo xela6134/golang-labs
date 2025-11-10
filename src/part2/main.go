3242d899fb6b5b1c7f106fc9de42fbb1d3394b56
alex lee <leol3o05@gmail.com>
2025-11-10 18:15:40 +0900

    scan

--- src/part2/main.go
+++ src/part2/main.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	var name = "Alex"
 	age := 200
-	const city = "Sydney"
+	const city string = "Sydney"
+	cost := 3.1
 
 	fmt.Println("Hello World")
 	fmt.Println(name, "is", age, "years old")
@@ -13,4 +14,5 @@ func main() {
 	name = "Joe"
 
 	fmt.Printf("%v lives in %v and is %v years old\n", name, city, age)
+	fmt.Printf("%T, %T, %T, %T\n", name, age, city, cost)
 }
--- src/part3/main.go
+++ src/part3/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	
+	var name string
+	var age int
+
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+
+	fmt.Print("Enter your age: ")
+	fmt.Scan(&age)
+
+	fmt.Printf("Hello %s, you are %d years old!\n", name, age)
+	fmt.Printf("%v (%T), %v (%T)\n", &name, &name, &age, &age)
 }
